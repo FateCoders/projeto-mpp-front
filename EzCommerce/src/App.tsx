9aabe74621757401c03dc79f83a4f934796d7ba8
Marlon Passos <101027484+MarlonVBP@users.noreply.github.com>
2025-05-30 00:13:22 -0300

    :package: Instalando o bootstrap icons

--- EzCommerce/src/App.tsx
+++ EzCommerce/src/App.tsx
@@ -1,7 +1,8 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/HomePage/homePage';
-import NotFound from './pages/NotFoundPage/NotFoundPage';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/HomePage/homePage";
+import NotFound from "./pages/NotFoundPage/NotFoundPage";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 export default function App() {
   return (
