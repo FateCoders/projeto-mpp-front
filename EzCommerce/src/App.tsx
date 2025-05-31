b19453faf9b735bd6c1f82232159481e1db961d7
Marlon Passos <101027484+MarlonVBP@users.noreply.github.com>
2025-05-31 00:09:31 -0300

    :sparkles: Adicionando novas rotas

--- EzCommerce/src/App.tsx
+++ EzCommerce/src/App.tsx
@@ -3,12 +3,16 @@ import Home from "./pages/HomePage/homePage";
 import NotFound from "./pages/NotFoundPage/NotFoundPage";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
+import ContactPage from "./pages/ContactPage/ContactPage";
+import Cartpage from "./pages/CartPage/CartPage";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/Contact" element={<ContactPage />} />
+        <Route path="/Cart" element={<Cartpage />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
