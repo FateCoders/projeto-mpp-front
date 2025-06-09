7ce264d365d6195ba6864d0dde9ef6ca78346899
Marlon Passos <101027484+MarlonVBP@users.noreply.github.com>
2025-06-09 13:05:51 -0300

    :sparkles: Enhance SignUpPage with PasswordInput component and form validation

--- EzCommerce/src/pages/SignUpPage/SignUpPage.tsx
+++ EzCommerce/src/pages/SignUpPage/SignUpPage.tsx
@@ -3,8 +3,10 @@ import HeaderComponent from "../../components/HeaderComponent/Header";
 import FooterComponent from "../../components/FooterComponent/Footer";
 import { Container, Row, Col, Card, Button, Form } from "react-bootstrap";
 import { motion, AnimatePresence } from "framer-motion";
+import PasswordInput from "../../components/PasswordInput/PasswordInput";
 import "../../App.css";
 import "./SignUpPage.css";
+import { Link } from "react-router-dom";
 
 export default function SignUpPage() {
   const [step, setStep] = useState(1);
@@ -13,6 +15,7 @@ export default function SignUpPage() {
     email: "",
     telefone: "",
     senha: "",
+    confirmPassword: "",
     cep: "",
     endereco: "",
     bairro: "",
@@ -42,189 +45,227 @@ export default function SignUpPage() {
   );
 
   const animationProps = {
-    initial: { opacity: 0, x: 50 }, // Começa da direita
-    animate: { opacity: 1, x: 0 }, // Vai para o centro
-    exit: { opacity: 0, x: -50 }, // Sai para a esquerda
-    transition: { duration: 0.1 },
+    initial: { opacity: 0, x: 50 },
+    animate: { opacity: 1, x: 0 },
+    exit: { opacity: 0, x: -50 },
+    transition: { duration: 0.2 },
   };
 
   return (
-    <>
-      <div className="full-page-layout contact-page">
-        <HeaderComponent />
-        <div className="full-page-content fade-in">
-          <Container className="py-5">
-            <Row className="justify-content-center">
-              <Col md={8}>
-                <Card className="p-4 shadow">
-                  <Card.Body>
-                    <h2 className="text-center mb-4">
-                      Bem-vindo ao EzCommerce
-                    </h2>
+    <div className="full-page-layout contact-page">
+      <HeaderComponent />
+      <div className="full-page-content fade-in">
+        <Container className="py-5">
+          <Row className="justify-content-center">
+            <Col md={8}>
+              <Card className="p-4 shadow">
+                <Card.Body>
+                  <h2 className="text-center mb-4">Bem-vindo ao EzCommerce</h2>
 
-                    <Form onSubmit={handleSubmit}>
-                      <AnimatePresence mode="wait">
-                        {step === 1 && (
-                          <motion.div key="step1" {...animationProps}>
-                            <Form.Group className="mb-3" controlId="nome">
-                              <Form.Label>Nome:</Form.Label>
-                              <Form.Control
-                                type="text"
-                                name="nome"
-                                value={formData.nome}
-                                onChange={handleChange}
-                                placeholder="Digite seu nome"
-                                required
-                              />
-                            </Form.Group>
+                  <Form onSubmit={handleSubmit}>
+                    <AnimatePresence mode="wait">
+                      {step === 1 && (
+                        <motion.div key="step1" {...animationProps}>
+                          <Form.Group className="mb-3" controlId="nome">
+                            <Form.Label>
+                              <i className="bi bi-person-fill me-2"></i>Nome:
+                            </Form.Label>
+                            <Form.Control
+                              type="text"
+                              name="nome"
+                              value={formData.nome}
+                              onChange={handleChange}
+                              placeholder="Digite seu nome completo"
+                              required
+                            />
+                          </Form.Group>
 
-                            <Form.Group className="mb-3" controlId="email">
-                              <Form.Label>Email:</Form.Label>
-                              <Form.Control
-                                type="email"
-                                name="email"
-                                value={formData.email}
-                                onChange={handleChange}
-                                placeholder="Digite seu email"
-                                required
-                              />
-                            </Form.Group>
+                          <Form.Group className="mb-3" controlId="email">
+                            <Form.Label>
+                              <i className="bi bi-envelope-fill me-2"></i>Email:
+                            </Form.Label>
+                            <Form.Control
+                              type="email"
+                              name="email"
+                              value={formData.email}
+                              onChange={handleChange}
+                              placeholder="Digite seu email"
+                              required
+                            />
+                          </Form.Group>
 
-                            <Form.Group className="mb-3" controlId="telefone">
-                              <Form.Label>Telefone:</Form.Label>
-                              <Form.Control
-                                type="tel"
-                                name="telefone"
-                                value={formData.telefone}
-                                onChange={handleChange}
-                                placeholder="Digite seu telefone"
-                                required
-                              />
-                            </Form.Group>
+                          <Form.Group className="mb-3" controlId="telefone">
+                            <Form.Label>
+                              <i className="bi bi-telephone-fill me-2"></i>
+                              Telefone:
+                            </Form.Label>
+                            <Form.Control
+                              type="tel"
+                              name="telefone"
+                              value={formData.telefone}
+                              onChange={handleChange}
+                              placeholder="Digite seu telefone"
+                              required
+                            />
+                          </Form.Group>
 
-                            <Form.Group className="mb-3" controlId="senha">
-                              <Form.Label>Senha:</Form.Label>
-                              <Form.Control
-                                type="password"
-                                name="senha"
-                                value={formData.senha}
-                                onChange={handleChange}
-                                placeholder="Crie uma senha"
-                                required
-                              />
-                            </Form.Group>
+                          <PasswordInput
+                            label={
+                              <>
+                                <i className="bi bi-lock-fill me-2"></i>Senha:
+                              </>
+                            }
+                            name="senha"
+                            value={formData.senha}
+                            onChange={handleChange}
+                          />
 
-                            <div className="text-end">
-                              <Button
-                                variant="primary"
-                                onClick={nextStep}
-                                disabled={
-                                  !formData.nome.trim() ||
-                                  !formData.email.trim() ||
-                                  !formData.telefone.trim() ||
-                                  !formData.senha.trim()
-                                }
-                              >
-                                Próximo
-                              </Button>
-                            </div>
-                          </motion.div>
-                        )}
+                          <PasswordInput
+                            label={
+                              <>
+                                <i className="bi bi-lock-fill me-2"></i>
+                                Confirmar Senha:
+                              </>
+                            }
+                            name="confirmPassword"
+                            value={formData.confirmPassword}
+                            onChange={handleChange}
+                          />
 
-                        {step === 2 && (
-                          <motion.div key="step2" {...animationProps}>
-                            <Form.Group className="mb-3" controlId="cep">
-                              <Form.Label>CEP:</Form.Label>
-                              <Form.Control
-                                type="text"
-                                name="cep"
-                                value={formData.cep}
-                                onChange={handleChange}
-                                placeholder="Digite seu CEP"
-                                required
-                              />
-                            </Form.Group>
+                          <div className="text-end">
+                            <Button
+                              variant="primary"
+                              onClick={nextStep}
+                              disabled={
+                                !formData.nome.trim() ||
+                                !formData.email.trim() ||
+                                !formData.telefone.trim() ||
+                                !formData.senha.trim() ||
+                                !formData.confirmPassword.trim() ||
+                                formData.senha !== formData.confirmPassword
+                              }
+                            >
+                              Próximo
+                            </Button>
+                          </div>
+                        </motion.div>
+                      )}
 
-                            <Form.Group className="mb-3" controlId="endereco">
-                              <Form.Label>Endereço:</Form.Label>
-                              <Form.Control
-                                type="text"
-                                name="endereco"
-                                value={formData.endereco}
-                                onChange={handleChange}
-                                placeholder="Digite seu endereço"
-                                required
-                              />
-                            </Form.Group>
+                      {step === 2 && (
+                        <motion.div key="step2" {...animationProps}>
+                          <Form.Group className="mb-3" controlId="cep">
+                            <Form.Label>
+                              <i className="bi bi-geo-alt-fill me-2"></i>CEP:
+                            </Form.Label>
+                            <Form.Control
+                              type="text"
+                              name="cep"
+                              value={formData.cep}
+                              onChange={handleChange}
+                              placeholder="Digite seu CEP"
+                              required
+                            />
+                          </Form.Group>
 
-                            <Form.Group className="mb-3" controlId="bairro">
-                              <Form.Label>Bairro:</Form.Label>
-                              <Form.Control
-                                type="text"
-                                name="bairro"
-                                value={formData.bairro}
-                                onChange={handleChange}
-                                placeholder="Digite seu bairro"
-                                required
-                              />
-                            </Form.Group>
+                          <Form.Group className="mb-3" controlId="endereco">
+                            <Form.Label>
+                              <i className="bi bi-house-door-fill me-2"></i>
+                              Endereço:
+                            </Form.Label>
+                            <Form.Control
+                              type="text"
+                              name="endereco"
+                              value={formData.endereco}
+                              onChange={handleChange}
+                              placeholder="Digite seu endereço"
+                              required
+                            />
+                          </Form.Group>
 
-                            <Form.Group className="mb-3" controlId="cidade">
-                              <Form.Label>Cidade:</Form.Label>
-                              <Form.Control
-                                type="text"
-                                name="cidade"
-                                value={formData.cidade}
-                                onChange={handleChange}
-                                placeholder="Digite sua cidade"
-                                required
-                              />
-                            </Form.Group>
+                          <Form.Group className="mb-3" controlId="bairro">
+                            <Form.Label>
+                              <i className="bi bi-signpost-2-fill me-2"></i>
+                              Bairro:
+                            </Form.Label>
+                            <Form.Control
+                              type="text"
+                              name="bairro"
+                              value={formData.bairro}
+                              onChange={handleChange}
+                              placeholder="Digite seu bairro"
+                              required
+                            />
+                          </Form.Group>
 
-                            <Form.Group className="mb-4" controlId="estado">
-                              <Form.Label>Estado:</Form.Label>
-                              <Form.Control
-                                type="text"
-                                name="estado"
-                                value={formData.estado}
-                                onChange={handleChange}
-                                placeholder="Digite seu estado"
-                                required
-                              />
-                            </Form.Group>
+                          <Form.Group className="mb-3" controlId="cidade">
+                            <Form.Label>
+                              <i className="bi bi-building me-2"></i>Cidade:
+                            </Form.Label>
+                            <Form.Control
+                              type="text"
+                              name="cidade"
+                              value={formData.cidade}
+                              onChange={handleChange}
+                              placeholder="Digite sua cidade"
+                              required
+                            />
+                          </Form.Group>
 
-                            <div className="d-flex justify-content-between">
-                              <Button variant="danger" onClick={backStep}>
-                                Anterior
-                              </Button>
-                              <Button
-                                variant="primary"
-                                type="submit"
-                                disabled={
-                                  !formData.cep.trim() ||
-                                  !formData.endereco.trim() ||
-                                  !formData.bairro.trim() ||
-                                  !formData.cidade.trim() ||
-                                  !formData.estado.trim()
-                                }
-                              >
-                                Finalizar Cadastro
-                              </Button>
-                            </div>
-                          </motion.div>
-                        )}
-                      </AnimatePresence>
+                          <Form.Group className="mb-4" controlId="estado">
+                            <Form.Label>
+                              <i className="bi bi-globe-americas me-2"></i>
+                              Estado:
+                            </Form.Label>
+                            <Form.Control
+                              type="text"
+                              name="estado"
+                              value={formData.estado}
+                              onChange={handleChange}
+                              placeholder="Digite seu estado"
+                              required
+                            />
+                          </Form.Group>
 
-                      {renderDots()}
-                    </Form>
-                  </Card.Body>
-                </Card>
-              </Col>
-            </Row>
-          </Container>
-        </div>
-        <FooterComponent />
+                          <div className="d-flex justify-content-between">
+                            <Button variant="danger" onClick={backStep}>
+                              Anterior
+                            </Button>
+                            <Button
+                              variant="primary"
+                              type="submit"
+                              disabled={
+                                !formData.cep.trim() ||
+                                !formData.endereco.trim() ||
+                                !formData.bairro.trim() ||
+                                !formData.cidade.trim() ||
+                                !formData.estado.trim()
+                              }
+                            >
+                              Finalizar Cadastro
+                            </Button>
+                          </div>
+                        </motion.div>
+                      )}
+                    </AnimatePresence>
+
+                    {renderDots()}
+                    <div className="text-center mt-3">
+                      <span>Já tem uma conta? </span>
+                      <Link
+                        to="/signin"
+                        className="text-primary fw-bold text-decoration-none"
+                      >
+                        Entrar
+                      </Link>
+                    </div>
+                  </Form>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
       </div>
-    </>
+      <FooterComponent />
+    </div>
   );
 }
