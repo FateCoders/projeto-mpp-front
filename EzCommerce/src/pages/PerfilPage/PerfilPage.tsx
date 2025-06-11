af4f05e51978b96c579b79ff9f3d0d9ef9b11e9f
Marlon Passos <101027484+MarlonVBP@users.noreply.github.com>
2025-06-11 15:14:22 -0300

    :art: Refactor PerfilPage layout; integrate Header and Footer components for improved structure

--- EzCommerce/src/pages/PerfilPage/PerfilPage.tsx
+++ EzCommerce/src/pages/PerfilPage/PerfilPage.tsx
@@ -1,8 +1,13 @@
+import FooterComponent from "../../components/FooterComponent/Footer";
+import HeaderComponent from "../../components/HeaderComponent/Header";
+
 export default function PerfilPage() {
     return (
-        <div className="d-flex align-items-center justify-content-center vh-100 fade-in">
-            <h1>Perfil Page</h1>
-            <p>Esta é a página de perfil do usuário.</p>
+        <div className="full-page-layout contact-page">
+            <HeaderComponent />
+            <div className="full-page-content fade-in">
+            </div>
+            <FooterComponent />
         </div>
     );
 }
